9ca3e4c381aa243c5b432ab7c83152876d48acc1
DustinBRiley <101905245+DustinBRiley@users.noreply.github.com>
2023-06-03 13:02:46 -0600

    Add files via upload

---
+++ DustinRileyAssignment1.py
@@ -0,0 +1,10 @@
+# INF360 - Programming in Python
+# Dustin Riley
+# Assignment 1
+
+print("Welcome!\n")
+print("How much wood could a woodchuck chuck if a woodchuck could chuck wood?\n")
+myinput = input()
+myinput = int(myinput)
+print("You said " + str(myinput) + "\n")
+print("Is a woodchuck just a beaver? " + str((myinput ** 7)/2) + " hopefully you didn't put in a huge number making the (x^7)/2 too big for maxint")
